96bb826b224b2b33bd262a2194508410723a15c6
ed <edsim97@gmail.com>
2023-03-05 13:49:20 +0300

    Data to separate file

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,27 +1,23 @@
 import { useState } from "react";
 import React from 'react';
 import "src/assets/styles/App.css";
+import getTasksData from "src/assets/tasks.data";
 import NavBar from "src/components/NavBar/NavBar";
 import TaskList from "src/components/TaskList/TaskList";
-import { Task } from "src/models/Task";
+import { TaskGroup } from "src/models/TaskGroup";
 
 function App() {
 
-    const [tasks, changeTasks] = useState<Task[]>([
-        {
-            id: "1",
-            name: "UI позиций для Tally",
-            description: "Для сопоставления определенных документов пользователям нужно сопоставлять позиции.Для сопоставления определенных документов пользователям нужно сопоставлять позиции.Для сопоставления определенных документов пользователям нужно сопоставлять позиции.Для сопоставления определенных документов пользователям нужно сопоставлять позиции.Для сопоставления определенных документов пользователям нужно сопоставлять позиции.Для сопоставления определенных документов пользователям нужно сопоставлять позиции.Для сопоставления определенных документов пользователям нужно сопоставлять позиции.Для сопоставления определенных документов пользователям нужно сопоставлять позиции.Для сопоставления определенных документов пользователям нужно сопоставлять позиции.Для сопоставления определенных документов пользователям нужно сопоставлять позиции.",
-            link: "https://entera.atlassian.net/browse/EN-1854"
-        },
-        { id: "2", name: "task2", description: "asdasdasdasdasfsad", link: "" },
-        { id: "3", name: "task3" },
-    ]);
+    const [taskGroups, changeTaskGroups] = useState<TaskGroup[]>(getTasksData());
 
     return (
         <div className="App">
             <NavBar />
-            <TaskList tasks={tasks} name={"Список задач 1"}/>
+            {
+                taskGroups?.map(
+                 group => <TaskList tasks={group.taskList} name={group.name} key={group.id}/>
+                )
+            }
         </div>
     );
 }
