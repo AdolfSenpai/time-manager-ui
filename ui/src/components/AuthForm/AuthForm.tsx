c4ad34414cf660db3d6096f7647186e9d37e2d9f
ed <edsim97@gmail.com>
2023-03-07 21:38:29 +0300

    Show password icon

--- ui/src/components/AuthForm/AuthForm.tsx
+++ ui/src/components/AuthForm/AuthForm.tsx
@@ -4,12 +4,17 @@ import styles from "src/components/AuthForm/AuthForm.module.scss";
 import React from "react";
 import EtmButton from "src/components/common/EtmButton/EtmButton";
 import EtmInput from "src/components/common/EtmInput/EtmInput";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default function AuthForm() {
 
     const [isRegistration, setIsRegistration] = useState<boolean>(false);
-
-
+    const [isPasswordShown, setIsPasswordShown] = useState<boolean>(false);
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [name, setName] = useState<string>("");
 
     return(
         <form className={styles.AuthForm}>
@@ -17,7 +22,13 @@ export default function AuthForm() {
             <CSSTransition in={isRegistration} timeout={300} unmountOnExit>
                 <EtmInput type="text" placeholder="Name" />
             </CSSTransition>
-            <EtmInput type="text" placeholder="Password" />
+            <div>
+                <EtmInput type="password" placeholder="Password" />
+                {
+                    ( isPasswordShown && <FontAwesomeIcon icon={faEyeSlash}/> )
+                    || <FontAwesomeIcon icon={faEye}/>
+                }
+            </div>
             <div className={styles.Buttons}>
                 <CSSTransition in={!isRegistration} timeout={300} unmountOnExit>
                     <EtmButton>Sign In</EtmButton>
