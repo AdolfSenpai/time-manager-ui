9468145e9751ed45c36fc3d7aea865a41eaab117
ed <edsim97@gmail.com>
2023-03-14 00:36:48 +0300

    passwordInput, inputError

--- src/components/common/EtmInput/EtmInput.tsx
+++ src/components/common/EtmInput/EtmInput.tsx
@@ -1,13 +1,54 @@
 import React from "react";
 import { InputHTMLAttributes } from "react";
 import styles from "src/components/common/EtmInput/EtmInput.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { MouseEventHandler } from "react";
 
-export default function EtmInput(props: InputHTMLAttributes<HTMLInputElement>) {
+type EtmInputAttributes = InputHTMLAttributes<HTMLInputElement> & {
+    fieldErrors?: (string | undefined)[]
+};
+
+export default function EtmInput({fieldErrors, ...props}: EtmInputAttributes) {
+
+    const [isPasswordShown, setIsPasswordShown] = useState<boolean>(false);
+
+    const togglePasswordShow: MouseEventHandler = (event) => {
+        event.preventDefault();
+        setIsPasswordShown(!isPasswordShown);
+    };
+
+    const getInputType = () => props.type === "password" && (isPasswordShown && "text" || "password") || props.type;
 
     return(
         <div className={styles.EtmInput}>
-            <input {...props}/>
+            <div className={styles.InputRow}>
+                <input {...props} type={getInputType()}/>
+                {
+                    props.type === "password" &&
+                    (
+                        isPasswordShown
+                        && <FontAwesomeIcon
+                            onClick={togglePasswordShow}
+                            className={styles.PasswordIcon}
+                            icon={faEye}
+                        />
+                        || <FontAwesomeIcon
+                            onClick={togglePasswordShow}
+                            className={styles.PasswordIcon}
+                            icon={faEyeSlash}
+                        />
+                    )
+                }
+            </div>
             <div className={styles.BorderBottom}/>
+            {
+                !!fieldErrors && fieldErrors.filter(Boolean).map(error =>
+                    <span key={error} className={styles.ErrorText}>{error}</span>
+                )
+            }
         </div>
     );
 }
